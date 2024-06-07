7d5cc2c6ebd67e2097b335455a3c7ad2c5c61580
aslanka <ayushlanka106@gmail.com>
2024-06-07 00:14:41 -0400

    inital commit

---
+++ src/constants/index.js
@@ -0,0 +1,547 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  typescript,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  docker,
+  carrent,
+  jobit,
+  tripguide,
+  cloudtek,
+  aksaSds,
+  deltaShoppe,
+  java,
+  angular,
+  laravel,
+  nextjs,
+  postgresql,
+  git,
+  mysimplerx,
+  vars,
+  flexigolf,
+  chotok,
+  portfolio,
+  ugap,
+  edfry,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full-Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Mobile App Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Front-end Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "NextJs",
+    icon: nextjs,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Angular",
+    icon: angular,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "Laravel",
+    icon: laravel,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "PostgreSQL",
+    icon: postgresql,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full stack Engineer",
+    company_name: "CloudTek",
+    icon: cloudtek,
+    iconBg: "#fff",
+    date: "June 2022 - Present",
+    points: [
+      "Designed, developed, and maintained complex web applications using technologies such as NodeJs,Angular, Java Spring, Spring Security, ReactJS, GraphQL, RxDB, REST API, PostgreSQL, and CouchDB Conducted code training and PR reviews to ensure code quality and consistency across the development team",
+      "Managed a team of developers, providing guidance and support to ensure successful project delivery • Facilitated agile project management methodologies such as Scrum, Kanban, and Agile to ensure efficient project planning, execution, and delivery",
+      "Collaborated with cross-functional teams to identify and implement technical solutions to complex business problems",
+      "Analyzed user requirements and designed efficient, user-friendly interfaces that optimized user experience",
+      "Maintained comprehensive documentation of software design and codebase to ensure efficient code maintenance and future scalability",
+      "Conducted testing and debugging of applications to ensure high-quality and error-free code",
+      "Stayed up-to-date with emerging technologies and trends in software development, constantly seeking ways to improve skills and contribute to the growth of the company.",
+    ],
+  },
+  {
+    title: "Software Engineer MEAN Stack",
+    company_name: "AKSA-SDS",
+    icon: aksaSds,
+    iconBg: "#E6DEDD",
+    date: "Aug 2020 - May 2022",
+    points: [
+      "Developed and maintained various branchless banking applications using MEAN stack technologies (MongoDB, Express, AngularJS, NodeJS) for clients such as EasyPaisa and Jazzcash. • Designed and implemented microservices architecture to improve scalability and maintainability of systems.",
+      "Worked on TPL Trakker maps application, using Leaflet JS to create customized maps for FoodPanda, providing an alternative maps service to Google maps for food delivery purposes.",
+      "Contributed to the development of Zarai Taraqiati Bank Limited, a banking application built on Angular.",
+      "Utilized various libraries such as D3js, Leaflet JS, and Three.js to create interactive data visualizations and enhance the user experience.",
+      "Conducted unit testing and integration testing of applications using Jest JS to ensure high-quality, error-free code.",
+      "Collaborated with cross-functional teams to identify and implement technical solutions to complex business problems.",
+      "Maintained comprehensive documentation of software design and codebase to ensure efficient code maintenance and future scalability.",
+      "Stayed up-to-date with emerging technologies and trends in software development, constantly seeking\n" +
+        "ways to improve skills and contribute to the growth of the company.",
+    ],
+  },
+  {
+    title: "Jr. PHP Developer",
+    company_name: "DeltaShoppe",
+    icon: deltaShoppe,
+    iconBg: "#fff",
+    date: "Mar 2017 - Aug 2020",
+    points: [
+      "Developed and maintained web applications using PHP frameworks such as Laravel and Lumen for backend APIs and Blade templates for frontend development.",
+      "Collaborated with designers, project managers, and other developers to deliver high-quality products on time.",
+      "Participated in code reviews to ensure code quality and maintainability.",
+      "Utilized Git for version control and collaborated with other developers to ensure code consistency and prevent conflicts.",
+      "Built responsive and mobile-friendly web applications that offer a seamless user experience across all devices.",
+      "Integrated third-party APIs and libraries to enhance the functionality of web applications.",
+      "Developed and maintained documentation of software design and codebase to ensure efficient code maintenance and future scalability.",
+      "Optimized legacy systems into optimized systems by optimizing MySQL queries and relational databases.",
+      "Maintained and optimized legacy databases with millions of records.",
+      "Integrated Google Maps into MySimpleRx pharmacy coupon web application.",
+      "Developed microservices architecture for branchless banking applications.",
+      "Worked on some of the biggest branchless banking systems in Pakistan, including EasyPaisa and Jazzcash.",
+      "Worked on financial web applications such as VARS and MySimpleRx",
+    ],
+  },
+];
+
+const testimonials = [
+  //   {
+  //     testimonial:
+  //       "Behlole is a web-app wizard, he made sure to deliver on time. Will be looking forward to hiring him again. Impressed with is grip on the technicalities involved in the dev-stack he is working in.",
+  //     name: "Areeb Ali",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/1.jpg",
+  //   },
+  //   {
+  //     testimonial: "Proactive and Professional way of working, Good seller",
+  //     name: "DeHypto",
+  //     designation: "Client",
+  //     company: "Fiverp",
+  //     image: "https://randomuser.me/api/portraits/men/2.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "Very fast and excellent delivery, enjoyed working with him, It's a pleasure working with him highly recommended.",
+  //     name: "SleeHom",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/3.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "Very perfect, made the right choice working with him, Thanks for the Good work, Highly recommended",
+  //     name: "Lerthert",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/4.jpg",
+  //   },
+  //   {
+  //     testimonial: "work was well done",
+  //     name: "bless7",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/5.jpg",
+  //   },
+  //   {
+  //     testimonial: "good job. proper work.",
+  //     name: "tjdavis",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/6.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "Amazing delivery and before time ! Kind, friendly and helpful. Would order again. Thank you :)",
+  //     name: "secretdoctor",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/7.jpg",
+  //   },
+  //   {
+  //     testimonial: "amazing work, give a try to this seller",
+  //     name: "mrhazwansalam",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/8.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "make my 3rd order with him, he is so friendly..... mostly recommended",
+  //     name: "mrhazwansalam",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/8.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "ordered with him 2nd time.....this guy is amazing in every aspect",
+  //     name: "mrhazwansalam",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/8.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "Just tell him the complete requirements and the guy is good to go! I've ordered a Theme Integration of Angular and delivery was fine enough.",
+  //     name: "usamaabrar22",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/9.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "It was a wonderful experience.. Me as a freelancer would recommend Mr Behlole. My work done before time and Also beautiful way of conversation.. Shall give him order soon.",
+  //     name: "Ali Imam",
+  //     designation: "Client",
+  //     company: "Fiver",
+  //     image: "https://randomuser.me/api/portraits/men/10.jpg",
+  //   },
+];
+
+const projects = [
+  {
+    name: "Portfolio",
+    description:
+      "behlole.github.io is a personal website showcasing my portfolio and projects. It offers a glimpse into my skills, experience, and creative work, providing an opportunity to explore  talents and accomplishments.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "threejs",
+        color: "red-text-gradient",
+      },
+      {
+        name: "vitejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "java",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "springBoot",
+        color: "red-text-gradient",
+      },
+      {
+        name: "microservices",
+        color: "blue-text-gradient",
+      },
+    ],
+
+    image: portfolio,
+    source_code_link: "https://behlole.github.io",
+  },
+  {
+    name: "MySimpleRx",
+    description:
+      "MySimplerx.com is a user-friendly website that simplifies medication management. It offers secure prescription storage, medication tracking, and refill requests, ensuring easy organization and adherence to treatment plans.",
+    tags: [
+      {
+        name: "angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "green-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "laravelLumen",
+        color: "red-text-gradient",
+      },
+      {
+        name: "microservices",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: mysimplerx,
+    source_code_link: "https://mysimplerx.com",
+  },
+  {
+    name: "VARS",
+    description:
+      "dedicated platform within MySimplerx.com that provides valuable insights and analytics for medication usage. It offers users comprehensive data and visualizations to track medication adherence, patterns, and trends, enabling informed decision-making for improved health management.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "green-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "laravelLumen",
+        color: "red-text-gradient",
+      },
+      {
+        name: "microservices",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: vars,
+    source_code_link: "https://vars.mysimplerx.com",
+  },
+  {
+    name: "FlexiGolf",
+    description:
+      "website that offers a flexible and convenient way to book and manage golf tee times in Australia. With a user-friendly interface, golf enthusiasts can easily search for available tee times at various courses, book their preferred slots, and even invite friends to join their game. FlexiGolf.com.au aims to streamline the golf booking process, providing golfers with a hassle-free experience and ensuring they make the most of their time on the greens.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "green-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "laravelLumen",
+        color: "red-text-gradient",
+      },
+      {
+        name: "microservices",
+        color: "blue-text-gradient",
+      },
+    ],
+
+    image: flexigolf,
+    source_code_link: "https://flexigolf.com.au",
+  },
+  {
+    name: "Chotok",
+    description:
+      "Chotok is a website that provides a convenient and hassle-free platform for online ticket booking. With a wide range of options for various events and activities, users can easily find and purchase tickets for their preferred entertainment and leisure experiences.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "green-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "laravelLumen",
+        color: "red-text-gradient",
+      },
+      {
+        name: "microservices",
+        color: "blue-text-gradient",
+      },
+    ],
+
+    image: chotok,
+    source_code_link: "https://chotok.com",
+  },
+  {
+    name: "UGAP",
+    description:
+      "ugap.net is a website that serves as a platform for the UGAP (Union des Groupements d'Achats Publics) organization. It offers comprehensive information and resources related to public procurement, providing government entities and public institutions with access to procurement solutions, contracts, and procurement advice.",
+    tags: [
+      {
+        name: "laravel",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mysql",
+        color: "pink-text-gradient",
+      },
+    ],
+
+    image: ugap,
+    source_code_link: "https://ugap.net",
+  },
+  {
+    name: "EdFry",
+    description:
+      "edfry.co is a website that provides a platform for booking mentorship sessions with Ed Fry. As a seasoned mentor, Ed offers guidance and expertise in various areas such as software development, design, and professional growth. Visitors can schedule sessions with Ed to gain valuable insights and advice to advance their careers.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "laravel",
+        color: "green-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "laravelLumen",
+        color: "red-text-gradient",
+      },
+      {
+        name: "microservices",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: edfry,
+    source_code_link: "https://edfry.co",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
